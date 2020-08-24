41c0006a54dc12bbe32f504b7b93555b79ff5ac9
ZOI-dayo <zoi@zoizoi.net>
2020-08-24 20:46:13 +0900

    Window & Run

--- src/main/java/net/zoizoi/java/mapeditorsvg/Main.java
+++ src/main/java/net/zoizoi/java/mapeditorsvg/Main.java
@@ -17,11 +17,22 @@ import org.w3c.dom.DOMImplementation;
 // SVG
 import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.apache.batik.svggen.SVGGraphics2D;
+// window
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
   public static void main(String[] args) throws Exception {
     createPNG();
     createSVG();
+
+    GameWindow gw = new GameWindow("テストウィンドウ",400,300);
+    DrawCanvas dc = new DrawCanvas();
+    gw.add(dc);
+    gw.setVisible(true);
   }
 
   /**
@@ -53,18 +64,24 @@ public class Main {
    * PNG (Portable Network Graphics) ファイルを出力します。
    */
   private static void createPNG() throws IOException {
+    // テンプレ
+    // サイズ指定
     int w = 297 * 2;
     int h = 210 * 2;
     BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
     Graphics2D g = image.createGraphics();
+    // 編集
     draw(g, w, h);
-    ImageIO.write(image, "png", new File("png.png"));
+    // 出力
+    ImageIO.write(image, "png", new File("sample.png"));
   }
 
   /**
    * SVG (Scalable Vector Graphics) ファイルを出力します。
    */
   private static void createSVG() throws IOException {
+    // テンプレ
+    // サイズ指定
     int w = 297 * 2;
     int h = 210 * 2;
     DOMImplementation domImpl = SVGDOMImplementation.getDOMImplementation();
@@ -72,7 +89,27 @@ public class Main {
     org.w3c.dom.Document doc = domImpl.createDocument(svgNS, "svg", null);
     SVGGraphics2D g = new SVGGraphics2D(doc);
     g.setSVGCanvasSize(new Dimension(w, h));
+    // 編集
     draw(g, w, h);
+    // 出力
     g.stream("svg.svg");
+  }
+}
+class GameWindow extends JFrame{
+
+  public GameWindow(String title, int width, int height) {
+    super(title);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(width,height);
+    setLocationRelativeTo(null);
+    setResizable(false);
+  }
+}
+class DrawCanvas extends JPanel{
+  Image img = Toolkit.getDefaultToolkit().getImage("sample.png");
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    //画像の表示
+    g.drawImage(img, 0, 0, this);
   }
 }
